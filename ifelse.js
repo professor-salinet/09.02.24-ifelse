5a6e6ef39d9d5ea1e3eeadb8fbe56dd75137ce6a
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-02-09 12:16:05 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+var x = 1;
+var y = 2;
+
+if (x == y) {
+    console.log("Números conferem!");
+} else {
+    console.log("Números divergem!");
+}
+
+/**
+ * DESAFIO:
+ * 
+ * Fazer aparecer a mensagem "Números conferem" no console
+ */
